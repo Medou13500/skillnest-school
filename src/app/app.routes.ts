a1e91b8fd6b783999a6723122d378e3b9a616072
nahteb13 <ethan.bermond@ynov.com>
2026-01-19 15:12:01 +0100

    page d'acceuil + front système connexion

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -9,5 +9,17 @@ export const routes: Routes = [
     path: '',
     redirectTo: 'home',
     pathMatch: 'full',
+  },  {
+    path: 'inscription',
+    loadComponent: () => import('./inscription/inscription.page').then( m => m.InscriptionPage)
   },
+  {
+    path: 'mot-de-passe-oublie',
+    loadComponent: () => import('./mot-de-passe-oublie/mot-de-passe-oublie.page').then( m => m.MotDePasseOubliePage)
+  },
+  {
+    path: 'connexion',
+    loadComponent: () => import('./connexion/connexion.page').then( m => m.ConnexionPage)
+  },
+
 ];
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { Router } from '@angular/router';
+import { IonicModule } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  templateUrl: './home.page.html',
+  styleUrls: ['./home.page.scss'],
+  standalone: true,
+  imports: [
+    IonicModule
+  ]
 })
 export class HomePage {
-  constructor() {}
+
+  constructor(private router: Router) {}
+
+  goToLogin(role: 'student' | 'parent') {
+    this.router.navigate(['/connexion'], {
+      state: { role }
+    });
+  }
 }
