eafb9f044ad660f764a003f01fd4ae6a861f2cdf
Edwiyyin <EdwinWehbe07@gmail.com>
2026-05-06 14:14:23 +0200

    merge

---
+++ .angular/cache/20.3.12/app/vite/deps/ios.transition-BDzw0_Hm-WUKPRWOI.js
@@ -0,0 +1,14 @@
+import {
+  iosTransitionAnimation,
+  shadow
+} from "./chunk-767BZFTW.js";
+import "./chunk-2VCEZZUU.js";
+import "./chunk-GRMK66PU.js";
+import "./chunk-XWQLWX4L.js";
+import "./chunk-LCMILTBF.js";
+import "./chunk-3FUEYPGI.js";
+import "./chunk-QHQP2P2Z.js";
+export {
+  iosTransitionAnimation,
+  shadow
+};
